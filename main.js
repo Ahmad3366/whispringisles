dddcfa93d988dcc20f3d53523e3e24cfddc4fb51
Ahmed <ahmaddx900@gmail.com>
2024-09-04 21:52:53 +0200

    fix bug

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import 'kaplay/global'
 import intro from "./scenes/intro";
 import loadAssets from "./loader";
 
+import worldMap from "./scenes/worldMap";
+import house from "./scenes/house";
+
 kaplay({
 	height: 720,
 	width: 1280,

1ce3ed5af4bbeaa483f74f3d9a6a3bcff10b2bc9
Ahmed <ahmaddx900@gmail.com>
2024-09-04 21:49:55 +0200

    write logic to fetch and display maps from tiled

--- utils.js
+++ utils.js
@@ -60,3 +60,80 @@ export function checkTransition(transition) {
 		})
 	}
 }
+
+export async function fetchMapData(path) {
+	const mapData = await fetch(path)
+	return await mapData.json()
+}
+
+export async function drawTiles(layer, tileset) {
+  let nbOfDrawnTiles = 0;
+  const tilePos = vec2(0, 0);
+  let tiles = []
+  for (const tile of layer.data) {
+    if (nbOfDrawnTiles % layer.width == 0) {
+      tilePos.x = 0
+      tilePos.y += 16
+    } else {
+      tilePos.x += 16
+    }
+
+    nbOfDrawnTiles++
+    if (tile == 0) continue
+
+    // const spriteData = getSprite('block')
+
+    tiles.push({
+      tileFrame: tile - 1,
+      x: tilePos.x,
+      y: tilePos.y,
+    })
+    // TODO add it inside the push method
+    // sprData: spriteData.data
+
+    // add([
+    //     sprite('tileset', { frame: tile - 1 }),
+    //     pos(tilePos),
+    //     offscreen({ hide: true })
+    // ])
+  }
+
+  onDraw(() => {
+
+    for (const tile of tiles) {
+
+      drawSprite({
+        sprite: tileset,
+        pos: vec2(tile.x, tile.y),
+        frame: tile.tileFrame
+      })
+      
+      // drawUVQuad({
+      //   pos: vec2(tile.x, tile.y),
+      //   width: 32,
+      //   height: 32,
+      //   tex: tile.sprData.tex,
+      //   quad: tile.sprData.frames[tile.tileFrame]
+      // })
+    }
+
+  })
+}
+
+export function drawColliders(layer) {
+  for (const object of layer.objects) {
+    add([
+      area({
+        shape: new Rect(
+          vec2(0),
+          object.width,
+          object.height
+        )
+      }),
+      pos(object.x, object.y + 16),
+      body({ isStatic: true }),
+      offscreen({ hide: true }),
+      object.name
+    ])
+  }
+}

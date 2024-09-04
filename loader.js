f23046234f1c29ef29e1b6379a41b5012c7f9976
Ahmed <ahmaddx900@gmail.com>
2024-09-04 21:49:08 +0200

    load tilesets

--- loader.js
+++ loader.js
@@ -1,4 +1,13 @@
 export default function loadAssets() {
 	loadBitmapFont("unscii", "./fonts/unscii_8x8.png", 8, 8)
 	loadSprite('logo', './sprites/Logo.png')
+
+	// loadSprite('overworld', 'Overworld.png', {
+	// 	sliceX: 40,
+	// 	sliceY: 36
+	// })
+	loadSprite('inner', 'Inner.png', {
+		sliceX: 40,
+		sliceY: 25
+	})
 }

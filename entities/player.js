3d5ba93c1541aed9ed035a1044cb6cf0a099a781
Ahmed <ahmaddx900@gmail.com>
2024-09-30 19:55:14 +0200

    add player animation

--- entities/player.js
+++ entities/player.js
@@ -1,29 +1,59 @@
 export function addPlayer(position) {
 	return add([
-		sprite('player'),
+		sprite('player', {anim: 'idle-down'}),
 		pos(position),
 		area({shape: new Rect(vec2(0, 8), 8, 4)}),
 		anchor('center'),
 		body(),
 		'player',
 		{
+			dir: 'down',
+			isMoving: false,
+			speed: 80,
+
+
 			add() {
 				this.onCollide('exit-house', () => {
 					go('worldMap')
 				})
 			},
+
+			update() {
+				if (this.isMoving) {
+					if (this.getCurAnim().name !== `walk-${this.dir}`) {
+						this.play(`walk-${this.dir}`)
+					}
+				} else {
+					this.play(`idle-${this.dir}`)
+				}
+			},
+
 			setMovment() {
 				onKeyDown('s', () => {
-					this.move(0, 90)
+					this.move(0, this.speed)
+					this.dir = 'down'
+					this.isMoving = true
 				})
 				onKeyDown('w', () => {
-					this.move(0, -90)
+					this.move(0, -this.speed)
+					this.dir = 'up'
+					this.isMoving = true
 				})
 				onKeyDown('a', () => {
-					this.move(-90, 0)
+					this.move(-this.speed, 0)
+					this.dir = 'side'
+					this.isMoving = true
+					this.flipX = true
 				})
 				onKeyDown('d', () => {
-					this.move(90, 0)
+					this.move(this.speed, 0)
+					this.dir = 'side'
+					this.isMoving = true
+					this.flipX = false
+				})
+
+				onKeyRelease(key => {
+					this.isMoving = false
 				})
 			}
 		}
